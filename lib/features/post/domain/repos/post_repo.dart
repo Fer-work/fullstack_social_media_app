ce8207a1a0f58786455463290c7506e5a1584741
rnaumi6618 <rnaumi6618@conestogac.on.ca>
2025-01-21 19:07:19 -0500

    functions for comment in firebase_post_repo,post.dart,post_repo.dart

--- lib/features/post/domain/repos/post_repo.dart
+++ lib/features/post/domain/repos/post_repo.dart
@@ -1,4 +1,5 @@
 import 'package:fullstack_social_media_app/features/post/domain/entities/post.dart';
+import 'package:fullstack_social_media_app/features/post/domain/entities/comment.dart';
 
 abstract class PostRepo {
   Future<List<Post>> fetchAllPosts();
@@ -6,4 +7,6 @@ abstract class PostRepo {
   Future<void> deletePost(String postId);
   Future<List<Post>> fetchUserPostsByUserId(String userId);
   Future<void> toggleLikePost(String postId, String userId);
+  Future<void> addComment(String postId, Comment comment);
+  Future<void> deleteComment(String postId, String commentId);
 }
